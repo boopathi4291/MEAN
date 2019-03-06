b83c204ae72bf12247b01294128284e49f666176
Boopathi Krishnan <boopathi.career@live.com>
2019-03-06 19:56:49 +0530

    Update users.js

--- express-server/routes/users.js
+++ express-server/routes/users.js
@@ -26,6 +26,36 @@ router.post('/register',(req,res)=>{
 });
 });
 
+router.post('/authenticate',(req,res)=>{
+    let token = req.body.token;
+    
+    if (token) {
+        if (token.startsWith('Bearer ')) {
+            // Remove Bearer from string
+            token = token.slice(7, token.length);
+          }
+        jwt.verify(token, config.secret, (err, decoded) => {
+          if (err) {
+            return res.json({
+              success: false,
+              message: 'Token is not valid'
+            });
+          } else {
+            req.decoded = decoded;
+            return res.json({
+                success: true,
+                message: 'Token is valid'
+              });
+          }
+        });
+      } else {
+        return res.json({
+          success: false,
+          message: 'Auth token is not supplied'
+        });
+      }
+});
+
 
 router.post('/login',(req,res)=>{
     let record = { email:req.body.email,password:req.body.password};
