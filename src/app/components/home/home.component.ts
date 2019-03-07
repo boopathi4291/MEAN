e0188c7543d86d3bb7544662cbc12a21c1b2c519
Boopathi Krishnan <boopathi.career@live.com>
2019-03-07 14:08:50 +0530

    Update home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from "./../../services/users.service";
+import {  HttpHeaders}    from '@angular/common/http';
+
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
-
-  constructor(private userService:UserService) { }
+  localStorage:Storage;
+  currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  token:any;
+  httpOptions:any;
+  
+  constructor(private userService:UserService) { 
+    
+  }
 
   ngOnInit() {
-      this.userService.getUsers().subscribe(res => console.log(res));
-
+    if(this.currentUser){
+        this.token = this.currentUser.token;
+        this.httpOptions = {
+            headers: new HttpHeaders({ 'Content-Type': 'application/json',"Authorization" :"Bearer "+this.token })
+          };
+    }
+    this.userService.getUsers(this.httpOptions).subscribe(res => console.log(res));
+    
   }
+
 
 }
