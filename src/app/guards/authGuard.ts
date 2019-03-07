8e21e723c23a3ff95267c94bbfb7d92370781777
Boopathi Krishnan <boopathi.career@live.com>
2019-03-07 10:16:14 +0530

    Add files via upload

---
+++ src/app/guards/authGuard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate,
+    ActivatedRouteSnapshot,
+    RouterStateSnapshot } from '@angular/router';
+import { AuthService } from "./../services/auth.service";
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService:AuthService){};
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean>|Promise<boolean>|boolean {
+      return this.authService.checkUser()
+  }
+
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class AuthService{
     constructor(private http:HttpClient, private router:Router){}
     localStorage:Storage;
@@ -9,14 +11,12 @@ export class AuthService{
     checkUser(){
         this.currentUser = JSON.parse(localStorage.getItem("currentUser"));
         if(this.currentUser){
-            let token = {token:'Bearer'+this.currentUser.token};
-            this.http.post<any>(`${this.uri}/authenticate`,token).subscribe((res:any)=>{
-                    console.log(res);
-                    return res.success;
-            })
+            let token = {token:'Bearer '+this.currentUser.token};
+           return this.http.post<any>(`${this.uri}/authenticate`,token);
         }
         else{
             this.router.navigate(['/login']);
+            
         }
     }
 }
