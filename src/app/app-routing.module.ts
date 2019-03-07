800cbaf4ef84aad37d02bce022f66e38ccce76f1
Boopathi Krishnan <boopathi.career@live.com>
2019-03-07 11:52:28 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,10 @@ import { HomeComponent } from "./components/home/home.component";
 import { RegisterComponent } from "./components/register/register.component";
 import { LoginComponent } from "./components/login/login.component";
 import { UnauthorizedComponent } from "./components/unauthorized/unauthorized.component";
+import { AuthGuard } from './guards/authGuard';
 
 const routes: Routes = [
-  {path:'',component:HomeComponent},
+  {path:'',component:HomeComponent, canActivate:[AuthGuard]},
   {path:'register',component:RegisterComponent},
   {path:'login',component:LoginComponent},
   {path:'unauthorized',component:UnauthorizedComponent},
